c6174a70fbac5acece4376a54a0b1fbec9ff42c8
debarshimaity2004 <debarshimaity14@gmail.com>
2025-12-01 21:25:17 +0530

    add dotenv file to the project

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+import dotenv from 'dotenv';
+
+dotenv.config({
+    path: './.env',
+});
+
+let myusername = process.env.MY_USERNAME;
+
+console.log("Username is: " + myusername);
 console.log("Start of my first backend project");
