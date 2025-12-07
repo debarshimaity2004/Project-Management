d59dfc18d65eeccc4f3cccea28930173c437f3db
debarshimaity2004 <debarshimaity14@gmail.com>
2025-12-07 17:54:19 +0530

    made express and cors configurations

--- src/app.js
+++ src/app.js
@@ -1,5 +1,20 @@
 import express from 'express';
+import cors from 'cors';
+
 const app = express();
+
+//app.use -> middleware....basic configuration
+app.use(express.json({limit: '16kb'}));
+app.use(express.urlencoded({ extended: true,limit: '16kb' }));
+app.use(express.static('public'));
+
+//cors configuration
+app.use(cors({
+    origin: process.env.CORS_ORIGIN?.split(",") || "https://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+}));
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
